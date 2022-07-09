16145b51b194e1cb6cc1f4ac2b7f9d79a6fb0c88
uen0_ <sarasa4869@zoho.com>
2022-07-09 16:15:13 +0900

    変更：SliderScriptに最小値とコメントの追加

--- Assets/Ueno/SliderScript.cs
+++ Assets/Ueno/SliderScript.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// スライダーを動かす処理
+/// </summary>
+
 public class SliderScript : MonoBehaviour
 {
     Slider _slider;
-
+  
+    /// <summary>最大値</summary>
     [SerializeField] float _maxValue = 100;
-
+    /// <summary>最小値</summary>
+    [SerializeField] float _minValue = 0;
+    /// <summary>現在値</summary>
     [SerializeField] float _currentValue;
+
     void Start()
     {
         _slider = GetComponent<Slider>();
         _slider.maxValue = _maxValue;
-        _currentValue = _maxValue;
+        _slider.minValue = _minValue;
     }
 
     // Update is called once per frame
