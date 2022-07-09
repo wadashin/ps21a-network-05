8eec2453838ca622436d2796a1cc3d74762d2a1a
uen0_ <sarasa4869@zoho.com>
2022-07-09 16:13:53 +0900

    変更：スライダーを動かす上での最低限の処理を入れた

--- Assets/Ueno/SliderScript.cs
+++ Assets/Ueno/SliderScript.cs
@@ -5,16 +5,21 @@ using UnityEngine.UI;
 
 public class SliderScript : MonoBehaviour
 {
+    Slider _slider;
 
+    [SerializeField] float _maxValue = 100;
 
+    [SerializeField] float _currentValue;
     void Start()
     {
-        
+        _slider = GetComponent<Slider>();
+        _slider.maxValue = _maxValue;
+        _currentValue = _maxValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _slider.value = _currentValue;
     }
 }
