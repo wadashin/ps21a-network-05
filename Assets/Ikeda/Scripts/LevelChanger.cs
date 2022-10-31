05edfec48950e3465e363fd2873703ea323e25aa
kaeru411r <ike269fr@gmail.com>
2022-10-31 11:49:54 +0900

    [変更] 人数の変更に合わせて敵の数、湧く間隔が変更できるように #50

--- Assets/Ikeda/Scripts/LevelChanger.cs
+++ Assets/Ikeda/Scripts/LevelChanger.cs
@@ -30,8 +30,19 @@ public class LevelChanger : MonoBehaviour
     [SerializeField] WaveData[] _waveData = default;
     [Tooltip("ウェーブマネージャー")]
     [SerializeField] WaveManager _waveManager = default;
-    [Tooltip("基本の人数")]
-    [SerializeField] int _defaultMenberNum = 4;
+    [Tooltip("人数ごとの最大出現数の倍率")]
+    [SerializeField] float[] _amountFactor = { 0.2f, 0.45f, 0.7f, 1f };
+    [Tooltip("人数ごとの出現間隔の倍率")]
+    [SerializeField] float[] _spawnTimeFactor = { 3f, 2.5f, 1.5f, 1f };
+    [SerializeField]
+    LevelFactor[] _levelFactors =
+    {
+        new(0.2f,3f),
+        new(0.45f,2.5f),
+        new(0.7f,1.5f),
+        new(1f,1f)
+
+    };
 
     /// <summary>参加中の人数</summary>
     public int MenberNum
@@ -46,16 +57,18 @@ public class LevelChanger : MonoBehaviour
 
     private void OnValidate()
     {
+
         WaveSet();
     }
 
     void WaveSet()
     {
         WaveData[] copy = (WaveData[])_waveData.Clone();
+        int index = Mathf.Min(_levelFactors.Length, _menberNum);
         for (int i = 0; i < copy.Length; i++)
         {
-            copy[i].MaxAmount = (int)(copy[i].MaxAmount * (_menberNum / (float)_defaultMenberNum));
-            copy[i].SpawnTime *= (float)_defaultMenberNum / _menberNum;
+            copy[i].MaxAmount = Mathf.CeilToInt(copy[i].MaxAmount * _levelFactors[index].Amount);
+            copy[i].SpawnTime *= Mathf.CeilToInt(_levelFactors[index].SpawnTime);
         }
         _waveManager.WaveData = copy;
     }
@@ -72,4 +85,17 @@ public class LevelChanger : MonoBehaviour
     {
 
     }
+
+    [System.Serializable]
+    struct LevelFactor
+    {
+        public LevelFactor(float amount, float spawnTime)
+        {
+            Amount = amount;
+            SpawnTime = spawnTime;
+        }
+        public float Amount;
+        public float SpawnTime;
+    }
 }
+
